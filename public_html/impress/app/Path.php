adb6a5ecc9cb4d494bd66a125f0e83bdd950a2f8
Ivan Martinez-Ortiz <imartinezortiz@gmail.com>
2020-01-30 22:34:02 +0100

    Path will be generated at build time and choosen randomly from available gamepaths.

--- public_html/impress/app/Path.php
+++ public_html/impress/app/Path.php
@@ -1,5 +1,5 @@
 <?php
-//This program has been developed by students from the bachelor Computer Science 
+//This program has been developed by students from the bachelor Computer Science
 //at Utrecht University within the Software and Game project course (2018)
 //©Copyright Utrecht University (Department of Information and Computing Sciences)
 namespace App;
@@ -10,7 +10,7 @@ class Path extends Model
 {
     //represents a path in the game:
     protected $fillable = ['problem_id', 'path'];
-    
+
    /**
     * Generates a random path for a given problem.
     *
@@ -20,13 +20,36 @@ class Path extends Model
     */
     public static function generatePath($problemid)
     {
-        //The path is generated in an external java file,
-        //because PHP has very bad datastructure support.
-        $filepath = base_path() . '/resources/pathgeneration/PathGeneration.jar';
-        $result = `java -jar $filepath 17 8 4 4 3`;
+        $result = self::getRandomGamePath();
 
         $path = Path::Create(['problem_id' => $problemid,
         'path' => $result]);
         return $path;
+    }
+
+    private static function getRandomGamePath()
+    {
+        $basePath = base_path() . DIRECTORY_SEPARATOR. 'resources'. DIRECTORY_SEPARATOR . 'pathgeneration';
+        $subdirs = self::getSubdirs($basePath);
+        $dir = rand(0, count($subdirs)-1);
+        $filepath = $subdirs[$dir] . DIRECTORY_SEPARATOR .'PathGeneration.txt';
+        $result = \file_get_contents($filepath);
+        return $result;
+    }
+
+    private static function getSubdirs($path)
+    {
+        $subdirs = array();
+        if ($handle = opendir($path)) {
+
+            while (false !== ($entry = readdir($handle))) {
+                if (is_dir($path.DIRECTORY_SEPARATOR.$entry) && substr($entry, 0, 1 ) != '.') {
+                    $subdirs[] = $path.DIRECTORY_SEPARATOR.$entry;
+                }
+            }
+
+            closedir($handle);
+        }
+        return $subdirs;
     }
 }
